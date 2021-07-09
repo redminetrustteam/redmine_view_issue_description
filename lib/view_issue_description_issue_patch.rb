029b39e2f550165b290485250c32dc34e07e9029
Jan Catrysse <23419642+jcatrysse@users.noreply.github.com>
2021-07-09 17:34:17 +0200

    Correct bad use of «user» parameter not in scope.

--- lib/view_issue_description_issue_patch.rb
+++ lib/view_issue_description_issue_patch.rb
@@ -5,7 +5,7 @@ module ViewIssueDescriptionIssuePatch
     def visible?(usr=nil)
       visible = super
 
-      unless (usr || User.current).admin? || self.author == user || user.is_or_belongs_to?(assigned_to) || !visible
+      unless (usr || User.current).admin? || self.author == (usr || User.current) || (usr || User.current).is_or_belongs_to?(assigned_to) || !visible
         visible = (usr || User.current).allowed_to?(:view_issue_description, self.project)
       end
       visible
