4de7dd42df41e6f51b5fa37ee0078032abdcdb16
Jan Catrysse <23419642+jcatrysse@users.noreply.github.com>
2021-07-09 10:51:41 +0200

    Remove obsolete files

--- after_init.rb
+++
@@ -1,3 +0,0 @@
-#Redmine::MenuManager.map :project_menu do |menu|
-#  menu.push :activity, { :controller => 'activities', :action => 'index' }, after: :overview, param: :project_id, :if => Proc.new {  |p| User.current.allowed_to?(:view_activities, p)  }
-#end
