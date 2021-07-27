466f0d8e713dc2be04e823914e7ae29cb3107a93
Jan Catrysse <23419642+jcatrysse@users.noreply.github.com>
2021-07-27 22:27:53 +0200

    Fix: activities menu wrongfully redirecting to "no project"

--- init.rb
+++ init.rb
@@ -47,7 +47,7 @@ Redmine::Plugin.register :redmine_view_issue_description do
   end
 
   Redmine::MenuManager.map :project_menu do |menu|
-    menu.push :activity, { :controller => 'activities', :action => 'index' }, after: :overview, param: :project_id, :if => Proc.new {  |p| User.current.allowed_to?(:view_activities, p)  }
+    menu.push :activity, { :controller => 'activities', :action => 'index' }, after: :overview, :if => Proc.new {  |p| User.current.allowed_to?(:view_activities, p)  }
   end
 
 end
